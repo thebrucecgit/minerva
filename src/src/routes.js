a0ee7a7b43a112562a54f7fd1a7796acee51cb45
thebrucecgit <brucecccccc@gmail.com>
2020-05-05 23:25:37 +1200

    Added code-splitting

--- src/src/routes.js
+++ src/src/routes.js
@@ -1,47 +1,50 @@
-import React from "react";
+import React, { Suspense, lazy } from "react";
 import { Switch, Route } from "react-router-dom";
 
-import Home from "./scenes/Home";
-import Pages from "./scenes/Pages";
-import Signups from "./scenes/Signups";
-import Auth from "./scenes/Auth";
+import Loader from "./components/Loader";
 
-import Dashboard from "./scenes/Dashboard";
+const Home = lazy(() => import("./scenes/Home"));
+const Pages = lazy(() => import("./scenes/Pages"));
+const Signups = lazy(() => import("./scenes/Signups"));
+const Auth = lazy(() => import("./scenes/Auth"));
+const Dashboard = lazy(() => import("./scenes/Dashboard"));
 
 function Routes({ authService }) {
   return (
-    <Switch>
-      <Route exact path="/">
-        <Home authService={authService} />
-      </Route>
+    <Suspense fallback={Loader}>
+      <Switch>
+        <Route exact path="/">
+          <Home authService={authService} />
+        </Route>
 
-      <Route
-        path="/auth"
-        render={({ match, location }) => (
-          <Auth match={match} location={location} authService={authService} />
-        )}
-      />
+        <Route
+          path="/auth"
+          render={({ match, location }) => (
+            <Auth match={match} location={location} authService={authService} />
+          )}
+        />
 
-      <Route
-        path="/signup"
-        render={({ match }) => (
-          <Signups match={match} authService={authService} />
-        )}
-      />
+        <Route
+          path="/signup"
+          render={({ match }) => (
+            <Signups match={match} authService={authService} />
+          )}
+        />
 
-      <Route
-        path="/dashboard"
-        render={({ match, location }) => (
-          <Dashboard
-            match={match}
-            location={location}
-            authService={authService}
-          />
-        )}
-      />
+        <Route
+          path="/dashboard"
+          render={({ match, location }) => (
+            <Dashboard
+              match={match}
+              location={location}
+              authService={authService}
+            />
+          )}
+        />
 
-      <Route path="/" render={({ match }) => <Pages match={match} />} />
-    </Switch>
+        <Route path="/" render={({ match }) => <Pages match={match} />} />
+      </Switch>
+    </Suspense>
   );
 }
 
