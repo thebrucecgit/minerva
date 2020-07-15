e684d62e2514370534e1f4e21d5e2a19dd3c673b
thebrucecgit <brucecccccc@gmail.com>
2020-07-15 12:34:29 +1200

    Throw AuthError when JWT expires

--- api/lib/authenticate.js
+++ api/lib/authenticate.js
@@ -1,9 +1,14 @@
 import jwt from "jsonwebtoken";
 import User from "./modules/user/model";
+import { AuthenticationError } from "apollo-server";
 
 const { JWT_SECRET } = process.env;
 
 export default async function (token) {
-  const { id } = jwt.verify(token, JWT_SECRET);
-  return await User.findById(id);
+  try {
+    const { id } = jwt.verify(token, JWT_SECRET);
+    return await User.findById(id);
+  } catch (e) {
+    throw new AuthenticationError("Must be authenticated");
+  }
 }
