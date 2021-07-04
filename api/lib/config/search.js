e969f2760b37d18e769d750cf33d8498514c3431
thebrucecgit <brucecccccc@gmail.com>
2021-07-04 22:00:20 +1200

    User profile edit updates propagate to algolia

--- api/lib/config/search.js
+++ api/lib/config/search.js
@@ -1,10 +1,28 @@
 import algoliasearch from "algoliasearch";
 import User from "../modules/user/model";
+import * as yup from "yup";
 
 const { ALGOLIA_APPID, ALGOLIA_ADMIN_API, ALGOLIA_INDEX } = process.env;
 
 const client = algoliasearch(ALGOLIA_APPID, ALGOLIA_ADMIN_API);
-export const index = client.initIndex(ALGOLIA_INDEX);
+const index = client.initIndex(ALGOLIA_INDEX);
+
+const indexSchema = yup.object().shape({
+  objectID: yup.string().required(),
+  name: yup.string(),
+  yearGroup: yup.number().integer(),
+  school: yup.string(),
+  biography: yup.string(),
+  academics: yup.array().of(yup.string()),
+  extras: yup.array().of(yup.string()),
+  pfp: yup.object().shape({
+    type: yup.string(),
+    url: yup.string(),
+    cloudinaryPublicId: yup.mixed(),
+  }),
+});
+
+export default index;
 
 export const FIELDS = "name yearGroup school biography academics extras pfp";
 
@@ -12,7 +30,7 @@ export function docToRecord(doc) {
   const tutor = { ...doc };
   tutor.objectID = doc._id;
   delete tutor._id;
-  return tutor;
+  return indexSchema.validateSync(tutor, { stripUnknown: true });
 }
 
 export async function addDocToRecord(doc) {
--- api/lib/modules/user/mutations/updateUser.js
+++ api/lib/modules/user/mutations/updateUser.js
@@ -1,6 +1,7 @@
 import { ApolloError } from "apollo-server";
 import User from "../model";
 import userSchema from "../yupSchema";
+import index, { docToRecord } from "../../../config/search";
 
 export default async function updateUser(_, updates, { user }) {
   if (updates.id !== user._id && user.userType !== "ADMIN")
@@ -12,6 +13,11 @@ export default async function updateUser(_, updates, { user }) {
   const updated = await User.findByIdAndUpdate(updates.id, updates, {
     new: true,
   });
+
+  // Update algolia index
+  if (updated.userType === "TUTOR") {
+    await index.saveObject(docToRecord(updated.toObject()));
+  }
 
   return updated;
 }
