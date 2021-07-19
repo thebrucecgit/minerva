be5e79a832f11aea9b8111955a5925dba8a2c978
thebrucecgit <brucecccccc@gmail.com>
2021-07-19 20:00:14 +1200

    DB auto connect to testing DB on testing env

--- api/lib/config/database.js
+++ api/lib/config/database.js
@@ -1,15 +1,16 @@
 import mongoose from "mongoose";
 
-const { DB_URI } = process.env;
+const { DB_URI, TEST_URI, NODE_ENV } = process.env;
 
 export async function connect(uri) {
-  await mongoose.connect(uri ?? DB_URI, {
+  await mongoose.connect(uri ?? (NODE_ENV === "test" ? TEST_URI : DB_URI), {
     useNewUrlParser: true,
     useFindAndModify: false,
     useCreateIndex: true,
     useUnifiedTopology: true,
   });
-  if (process.env.NODE_ENV != "test") console.log("Database is connected");
+  if (process.env.NODE_ENV !== "test") console.log("Database is connected");
+  return mongoose.connection.db;
 }
 
 export async function disconnect() {
