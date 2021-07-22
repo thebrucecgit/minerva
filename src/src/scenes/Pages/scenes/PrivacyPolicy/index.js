06f6fffa88ae6f898e64b4d7331e4a60b8e043a5
thebrucecgit <brucecccccc@gmail.com>
2021-07-22 13:20:52 +1200

    Update Privacy Policy

--- src/src/scenes/Pages/scenes/PrivacyPolicy/index.js
+++ src/src/scenes/Pages/scenes/PrivacyPolicy/index.js
@@ -16,23 +16,14 @@ function PrivacyPolicy() {
       <h2>Information collected&nbsp;</h2>
       <p>
         Academe&rsquo;s website may collect personal information such as names,
-        addresses, email addresses, telephone numbers, bank details, and/or
-        tutoring interests. Furnishing the specifically designated information
-        requested on our tutoring/tutee forms, including name, full address,
-        email address, and credit card payment information is necessary for
-        online purchases and to process all tax-deductible donations. Full names
-        and addresses are also collected for purposes of fulfilling brochure
-        requests via the online form.&nbsp;
+        addresses, email addresses, telephone numbers, and/or tutoring
+        interests.&nbsp;
       </p>
 
       <p>
         In addition, email addresses and tutoring/tutee interests are necessary
-        to process all tutoring/tutee requests. Because our database is large
-        and we have numerous name duplications (i.e. John Williams), we collect
-        mailing addresses as well to enable us to match the correct email
-        address and requested information with the correct name file (John
-        Williams) in our database. Anyone can choose to be removed from our
-        tutoring/tutee list at any time.&nbsp;
+        to process all tutoring/tutee requests. Anyone can choose to be removed
+        from our tutoring/tutee list at any time.&nbsp;
       </p>
 
       <p>
@@ -44,35 +35,18 @@ function PrivacyPolicy() {
       </p>
       <h2>Use of collected information&nbsp;</h2>
       <p>
-        Academe may use personal information such as name as mailing address
-        collected from websites for the purpose of future communication back to
-        online enrollees, in order to keep you informed of additional Academe
-        activities or other related types of events, but only if you are
-        provided the opportunity to opt out of that type of use.&nbsp;
-      </p>
-
-      <p>
-        Your bank details will be kept secure and only used for appropriately
-        charging for online purchases. Cancellations of financial subscriptions
-        should be made three days prior to the renewal of the subscription. Once
-        a cancellation is made, Academe will not retain your bank details unless
-        you specifically request this.&nbsp;&nbsp;
-      </p>
-
-      <p>
-        Academe may use personal information such as email address for the
-        purpose of future communication back to online enrollees regarding a
-        current or prior online purchase of tutoring. With the exception of the
-        initial confirmation email regarding an online purchase, you are given
-        the opportunity to opt-out of any further business email communications
-        from Academe.&nbsp;
+        Academe may use personal information such as name for the purpose of
+        future communication back to online enrollees, in order to keep you
+        informed of additional Academe activities or other related types of
+        events, but only if you are provided the opportunity to opt out of that
+        type of use.&nbsp;
       </p>
 
       <p>
         Academe only sends mail listing marketing communications and offers only
         to those who specifically opt-in to the service and submit their emails
         to us for that purpose. Anyone can choose to be removed from our Email
-        Club mailing list at any time. We will not sell, trade or share email
+        mailing list at any time. We will not sell, trade or share email
         addresses to/with companies or other organizations without your
         expressed permission.&nbsp;
       </p>
@@ -86,18 +60,21 @@ function PrivacyPolicy() {
       </p>
 
       <p>
-        Academe sites may use "cookies" in order to deliver web content specific
-        to individual users' interests or to keep track of online purchasing
-        transactions. Sensitive personal information is not stored within
-        cookies.&nbsp;
+        Academe sites may use third party "cookies" from{" "}
+        <a href="https://clicky.com/" target="_BLANK" rel="noreferrer">
+          Clicky
+        </a>{" "}
+        purely for statistical purposes. Sensitive personal information is not
+        stored within cookies.&nbsp;
       </p>
       <h2>Access to your own information</h2>
       <p>
         Questions regarding users' rights to review, modify or delete their
         previously provided personal information should be directed to Academe
-        via mail at <a href="mailto:admin@academe.co.nz">admin@academe.co.nz</a>
-        , or via phone at 021-170-2726. Any disputes will be resolved under
-        existing records regulations applicable to Academe.&nbsp;
+        via email at{" "}
+        <a href="mailto:admin@academe.co.nz">admin@academe.co.nz</a>, or via
+        phone at 021-170-2726. Any disputes will be resolved under existing
+        records regulations applicable to Academe.&nbsp;
       </p>
       <h2>How is the information used?</h2>
       <p>
@@ -132,51 +109,7 @@ function PrivacyPolicy() {
       </p>
       <p>
         If you no longer wish to be contacted for marketing purposes, please
-        email or write to the address shown below.
-      </p>
-      <h2>Employees</h2>
-      <p>
-        Personal information provided to us through the submission of an
-        employment application will be used for the analysis of suitability for
-        a position available.
-      </p>
-      <p>What sorts of personal information do we ask for?</p>
-      <ul>
-        <li>
-          Identifying and contact information (name, address, phone, email etc)
-        </li>
-        <li>
-          A CV and a cover letter outlining your job history, experience,
-          education and skills as they relate to the position applied for.
-        </li>
-        <li>
-          Due to the nature of our business, some positions require extra
-          information to be provided or obtained to ensure compliance with child
-          protection legislation.
-        </li>
-        <li>
-          If hired, banking, Inland Revenue, pension, and other related
-          information.
-        </li>
-      </ul>
-      <p>
-        If you apply to work for Academe, we will only use the information you
-        supply to us to process your application and to monitor recruitment
-        statistics. Where we want to disclose information to a third party, for
-        example where we want to take up a reference or obtain a
-        &lsquo;disclosure&rsquo; from the Criminal Records Bureau we will not do
-        so without informing you beforehand unless required by law.
-      </p>
-      <p>
-        If your application is successful your personal details will form part
-        of an employee record. The Academe group will only disclose this
-        information when required by law to authorized government agencies (e.g.
-        Inland Revenue Office), or otherwise as directed by yourself.
-      </p>
-      <p>
-        Limited personal information may be made available internally within the
-        Academe group to other employees or managers via organization employee
-        contact lists whether hard copy or on the organization intranet.
+        email <a href="mailto:admin@academe.co.nz">admin@academe.co.nz</a>.
       </p>
     </div>
   );
