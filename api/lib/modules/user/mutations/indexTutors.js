efcdfc3d05895fd7bfa13f7a3eb0935dde152d2c
thebrucecgit <brucecccccc@gmail.com>
2021-11-04 15:32:23 +1300

    Fix reindex

--- api/lib/modules/user/mutations/indexTutors.js
+++ api/lib/modules/user/mutations/indexTutors.js
@@ -1,7 +1,7 @@
 import { indexAll } from "../../../config/search";
 import { assertAdmin } from "../../../helpers/permissions";
 
-export default async function indexTutors(_, __, user) {
+export default async function indexTutors(_, __, { user }) {
   assertAdmin(user);
   await indexAll();
 }
