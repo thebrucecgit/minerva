f84d3e3b3d41256e1e31ed4774d93bf543943eec
Bruce C <brucecccccc@gmail.com>
2022-03-20 15:39:01 +1300

    Enlarge pfp to be 350px

--- api/lib/helpers/getAvatar.js
+++ api/lib/helpers/getAvatar.js
@@ -3,7 +3,7 @@ import querystring from "querystring";
 export default function getAvatar(name) {
   if (!name) throw new Error("No name");
   return `https://ui-avatars.com/api/?${querystring.stringify({
-    size: "256",
+    size: "350",
     background: "a9e4f5",
     name,
   })}`;
