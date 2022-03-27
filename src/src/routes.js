b522848c4893930d9ed5dc91b480246f6ccc1cc4
Bruce C <brucecccccc@gmail.com>
2022-03-27 17:53:37 +1300

    Ammend prev commit

--- src/src/routes.js
+++ src/src/routes.js
@@ -1,5 +1,5 @@
 import React, { Suspense, lazy, useEffect } from "react";
-import { Switch, Route, useLocation, Redirect } from "react-router-dom";
+import { Switch, Route, useLocation } from "react-router-dom";
 
 import Loader from "./components/Loader";
 import { loader } from "./styles/Loader.module.scss";
