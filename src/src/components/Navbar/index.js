4a0a25f839bbb975e76fa2122ed79d7f6f2541a8
Bruce C <brucecccccc@gmail.com>
2022-04-23 20:24:53 +1200

    Replace with Minerva logo

--- src/src/components/Navbar/index.js
+++ src/src/components/Navbar/index.js
@@ -1,5 +1,5 @@
 import { Link } from "react-router-dom";
-import logo from "../../media/logo.png";
+import logo from "../../media/logo.webp";
 import styled from "styled-components";
 
 const StyledNavbar = styled.nav`
