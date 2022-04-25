dc54a466c8293a1da1f11ae01d5d3bb5e3ba86a6
Bruce C <brucecccccc@gmail.com>
2022-04-25 13:29:13 +1200

    Remove redundant import

--- api/lib/modules/file/resolvers.js
+++ api/lib/modules/file/resolvers.js
@@ -2,7 +2,6 @@ import { GetObjectCommand, PutObjectCommand } from "@aws-sdk/client-s3";
 import { nanoid } from "nanoid";
 import { getSignedUrl } from "@aws-sdk/s3-request-presigner";
 import client from "../../config/s3";
-import { assertAdmin } from "../../helpers/permissions";
 
 const expiresIn = 60 * 60 * 24; // 24 hrs;
 
