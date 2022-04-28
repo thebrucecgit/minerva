3bcfd510718bfa7d5f399dc3f0afb1b26350b2e0
Bruce C <brucecccccc@gmail.com>
2022-04-28 22:34:14 +1200

    Change agenda to process every 5 minutes

--- api/lib/agenda.js
+++ api/lib/agenda.js
@@ -3,7 +3,7 @@ import Agenda from "agenda";
 import email from "./jobs/email";
 import search from "./jobs/search";
 
-const agenda = new Agenda();
+const agenda = new Agenda({ processEvery: "5 minutes" });
 
 export async function agendaSetup(mongo) {
   agenda.mongo(mongo);
