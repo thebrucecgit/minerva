f1295b77d7b688ef79ead3c63996c26fd23600b5
Bruce C <brucecccccc@gmail.com>
2022-04-29 21:45:28 +1200

    Cleanup GoogleLogin

--- src/src/components/GoogleLogin.js
+++ src/src/components/GoogleLogin.js
@@ -1,11 +1,10 @@
-import { useRef, useEffect, useState } from "react";
+import { useRef } from "react";
 import useScript from "hooks/useScript";
 
 export default function GoogleLogin({
   onGoogleSignIn = () => {},
   text = "signin_with",
 }) {
-  const [loaded, setLoaded] = useState(false);
   const googleSignInButton = useRef(null);
 
   useScript("https://accounts.google.com/gsi/client", () => {
@@ -13,15 +12,11 @@ export default function GoogleLogin({
       client_id: process.env.REACT_APP_GOOGLE_CLIENT_ID,
       callback: onGoogleSignIn,
     });
-    setLoaded(true);
-  });
-  useEffect(() => {
-    if (!loaded) return;
     window.google.accounts.id.renderButton(
       googleSignInButton.current,
       { theme: "outline", size: "large", text, width: "250" } // customization attributes
     );
-  }, [loaded, text]);
+  });
 
   return <div className="test" ref={googleSignInButton}></div>;
 }
