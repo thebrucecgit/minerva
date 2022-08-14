d4e83e0c426552972b7ed6e429243bf2079e665e
Bruce C <brucecccccc@gmail.com>
2022-08-14 16:03:10 +1200

    Chat message toast now links to chats page

--- src/src/scenes/Dashboard/hooks/useChat.js
+++ src/src/scenes/Dashboard/hooks/useChat.js
@@ -1,27 +1,36 @@
 import { useEffect } from "react";
 import useWebSocket from "./useWebSocket";
-import { useRouteMatch } from "react-router-dom";
+import { useHistory, useRouteMatch } from "react-router-dom";
 import { toast } from "react-toastify";
 
 export default function useChat(currentUser) {
   const ws = useWebSocket(currentUser);
 
   const match = useRouteMatch("/dashboard/chats");
+  const history = useHistory();
 
   useEffect(() => {
     function inboxHandler({ events }) {
       if (events.length > 3) {
-        toast(`You have ${events.length} new messages.`);
+        toast(`You have ${events.length} new messages.`, {
+          onClick: () => history.push(`/dashboard/chats`),
+        });
       } else {
         events.forEach((event) => {
           if (event.type === "MESSAGE") {
-            toast(event.text);
+            toast(event.text, {
+              onClick: () => history.push(`/dashboard/chats/${event.channel}`),
+            });
           }
         });
       }
     }
+
     function messageHandler(msg) {
-      if (!match) toast(`${msg.authorName}: ${msg.text}`);
+      if (match) return;
+      toast(`${msg.authorName}: ${msg.text}`, {
+        onClick: () => history.push(`/dashboard/chats/${msg.channel}`),
+      });
     }
     ws.bind("INBOX", inboxHandler);
     ws.bind("MESSAGE", messageHandler);
@@ -29,7 +38,7 @@ export default function useChat(currentUser) {
       ws.unbind("INBOX", inboxHandler);
       ws.unbind("MESSAGE", messageHandler);
     };
-  }, [match, ws]);
+  }, [match, ws, history]);
 
   return ws;
 }
